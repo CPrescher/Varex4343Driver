c18cfad10d631c53c50d4207fec53b4aaee9d707
Clemens Prescher <clemens.prescher@gmail.com>
2020-09-22 14:13:35 +0200

    Added some form of error handling for handling commands

--- Varex4343Driver/src/handle.cpp
+++ Varex4343Driver/src/handle.cpp
@@ -12,75 +12,97 @@ using std::string;
 constexpr int c_recv_buffer_size = 100;
 
 void handle_connection(SOCKET s) 
-// handles the communication between client and our server
+// handles the communication between client and the Varex control server
 // it is awaiting commands from the client and acts accordingly
 {
 	string ip = get_client_ip(s);
 
 	std::cout << ip << " connected. Waiting for Message." << std::endl;
-	send(s, "Welcome! You have connected to BANANA SERVER!", 46, NULL);
 
 	char recv_buffer[c_recv_buffer_size];
 	while (recv(s, recv_buffer, c_recv_buffer_size - 1, NULL) > 0) {
-
-		vector<string> recv_parts = read_command(recv_buffer);
-		int det_index = stoi(recv_parts[1]);
-		string command = recv_parts[2];
-
-		if (command == "set") {
-			string parameter = recv_parts[3];
-			if (parameter == "gain") {
-				int value = stoi(recv_parts[4]);
-				varex::set_gain(det_index, value);
-			}
-			else if (parameter == "exposure_time") {
-				double value = stod(recv_parts[4]);
-				varex::set_exposure_time(det_index, value);
-			}
-			else if (parameter == "streaming_target") {
-				varex::set_streaming_target(
-					det_index,
-					recv_parts[4].c_str(),
-					recv_parts[5].c_str()
-				);
-			}
+		string command = read_command(recv_buffer);
+		try {
+			handle_command(command);
+			send_success(s);
 		}
-		else if (command == "get") {
-			string parameter = recv_parts[3];
-			if (parameter == "status") {
-				int status = varex::get_status(det_index);
-			}
-			else if (parameter == "gain") {
-				int gain = varex::get_gain(det_index);
-			}
-			else if (parameter == "exposure_time") {
-				double exposure_time = varex::get_exposure_time(det_index);
-			}
-			else if (parameter == "streaming_target") {
-				string target = varex::get_streaming_target(det_index); 
-			}
-		}
-		else if (command == "start_acquisition") {
-			varex::start_collection(det_index);
-		}
-		else if (command == "stop_acquisition") {
-			varex::stop_collection(det_index);
-		}
-		else {
-
-		}
-
+		catch(const std::exception& e) {
+			std::cout << "Unable to understand command: " << command << std::endl;
+			std::cout << "Exception: " << e.what() << std::endl;
+			send_failed(s);
+		};
 	}
 	std::cout << "Connection was closed by the client" << std::endl;
 }
 
-vector<string> read_command(const char* recv_buffer) {
+void handle_command(const string& command) {
+	vector<string> command_parts = split_command(command);
+	int det_index = stoi(command_parts.at(1));
+	string verb = command_parts.at(2);
+
+	if (verb == "set") {
+		set_param(det_index, command_parts);
+	}
+	else if (verb == "get") {
+		get_param(det_index, command_parts);
+	}
+	else if (verb == "start_acquisition") {
+		varex::start_collection(det_index);
+	}
+	else if (verb == "stop_acquisition") {
+		varex::stop_collection(det_index);
+	}
+	else {
+		std::cout << "Unable to understand command: " << command << std::endl;
+	}
+}
+
+void set_param(int det_index, const vector<string>& command_parts) {
+	string parameter = command_parts.at(3);
+	if (parameter == "gain") {
+		int value = stoi(command_parts.at(4));
+		varex::set_gain(det_index, value);
+	}
+	else if (parameter == "exposure_time") {
+		double value = stod(command_parts.at(4));
+		varex::set_exposure_time(det_index, value);
+	}
+	else if (parameter == "streaming_target") {
+		varex::set_streaming_target(
+			det_index,
+			command_parts.at(4).c_str(),
+			command_parts.at(5).c_str()
+		);
+	}
+}
+
+void get_param(int det_index, const vector<string>& command_parts) {
+	string parameter = command_parts.at(3);
+	if (parameter == "status") {
+		int status = varex::get_status(det_index);
+	}
+	else if (parameter == "gain") {
+		int gain = varex::get_gain(det_index);
+	}
+	else if (parameter == "exposure_time") {
+		double exposure_time = varex::get_exposure_time(det_index);
+	}
+	else if (parameter == "streaming_target") {
+		string target = varex::get_streaming_target(det_index);
+	}
+	// still needs to send it back...
+}
+
+string read_command(const char* recv_buffer) {
 	std::istringstream recv_stream(recv_buffer);
 	string recv_str;
 	std::getline(recv_stream, recv_str, ';');
-	return split(recv_str, ':');
+	return recv_str;
 }
 
+vector<string> split_command(const string& command) {
+	return split(command, ':');
+}
 
 vector<string> split(const string& str, char delimiter)
 {
@@ -92,4 +114,12 @@ vector<string> split(const string& str, char delimiter)
 		tokens.push_back(token);
 	}
 	return tokens;
+}
+
+void send_success(SOCKET s) {
+	send(s, "1", 1, NULL);
+}
+
+void send_failed(SOCKET s) {
+	send(s, "0", 1, NULL);
 }
--- Varex4343Driver/src/handle.h
+++ Varex4343Driver/src/handle.h
@@ -6,5 +6,14 @@
 
 void handle_connection(SOCKET s);
 
-std::vector<std::string> read_command(const char* recv_buffer);
+void handle_command(const std::string& command);
+void set_param(int det_index, const std::vector<std::string>& command_parts);
+void get_param(int det_index, const std::vector<std::string>& command_parts);
+
+
+std::string read_command(const char* recv_buffer);
+std::vector<std::string> split_command(const std::string& command);
 std::vector<std::string> split(const std::string& str, char delimiter);
+
+void send_success(SOCKET s);
+void send_failed(SOCKET s);
